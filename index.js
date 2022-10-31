e69062b911730781b006291180cbc00526b6dd34
limji <limji@dgu.ac.kr>
2022-10-31 16:04:47 +0900

    commit

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const axios = require('axios');
 const cors = require('cors');
 
 const port = process.env.PORT || 3000;
+const apiRouter = express.Router();
+const app = express();
 
-let app = express();
 app.use(express.json());
 app.use(cors());
+app.disable('etag');
 
-app.get('/api', function(req, res) {
+apiRouter.all('/timeTable', function(req, res) {
     const key = '5c21b59a1000460eb087316813224fb5';
-    
+    let nextMonday = new Date();
+    nextMonday.setDate(nextMonday.getDate() + (((1 + 7 - nextMonday.getDay()) % 7) || 7));
+    console.log(nextMonday)
     let today = new Date().toISOString().substring(0, 10).replace(/-/g,'');
     axios({
         method: 'get',
@@ -32,10 +36,26 @@ app.get('/api', function(req, res) {
         }
     })
     .then(response => {
+        res.send({
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "테스트"
+                        }
+                    }
+                ]
+            }
+        });
         console.log(response.data['misTimetable'][1]['row'][0]['PERIO']);
         console.log(response.data['misTimetable'][1]['row'][0]['ITRT_CNTNT']);
         console.log(response.data['misTimetable'][1]['row'][0]['ITRT_CNTNT']);
     });
+});
+
+apiRouter.all('/mealInfo', function(req, res) {
+    date = req.body['detailParams']['date']['origin'].replace('-', '');
     axios({
         method: 'get',
         url: 'https://open.neis.go.kr/hub/mealServiceDietInfo',
@@ -46,8 +66,9 @@ app.get('/api', function(req, res) {
             'pSize': '35',
             'ATPT_OFCDC_SC_CODE': 'J10',
             'SD_SCHUL_CODE': '7621038',
-            'MLSV_FROM_YMD': '20221031',
-            'MLSV_TO_YMD': '20221031'
+            'MLSV_YMD': date
+            // 'MLSV_FROM_YMD': '20221031',
+            // 'MLSV_TO_YMD': '20221031'
         },
         headers: {
             'Content-Type': 'application/json;charset=UTF-8'
@@ -60,7 +81,7 @@ app.get('/api', function(req, res) {
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": response
+                            "text": response.data['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']
                         }
                     }
                 ]
